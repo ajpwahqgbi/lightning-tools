f9a46e9fc9b325a67ab8aee59325a1b3042cce0c
ajpwahqgbi <twabbiqjp@protonmail.com>
2021-05-10 19:54:10 +0000

    jitterbug: Add HTLC jitter injector

---
+++ clightning-plugins/jitterbug/README.md
@@ -0,0 +1,5 @@
+Jitterbug
+----
+This plugin adds jitter to all HTLC forwards. By default no more than 5.555 seconds delay will ever be added, but this is configurable in the plugin source code. 
+
+The delay added to each HTLC is not likely to withstand serious statistical analysis. Improvements and references to relevant literature would be greatly appreciated.
---
+++ clightning-plugins/jitterbug/jitterbug.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from pyln.client import Plugin
+import time
+import secrets
+
+plugin = Plugin()
+plugin.max_delay = secrets.randbelow(5) + secrets.randbelow(50) + secrets.randbelow(500) + secrets.randbelow(5000)
+
+@plugin.hook("htlc_accepted")
+def htlc_accepted(plugin: Plugin, openchannel: dict, **kwargs):
+    sleepytime = secrets.randbelow(plugins.max_delay)
+    time.sleep(sleepytime / 1000.0)
+    return {"result": "continue"}
+
+@plugin.init()
+def init(options: dict, configuration: dict, plugin: Plugin, **kwargs):
+    plugin.log(f"Plugin jitterbug initialized")
+
+plugin.run()
