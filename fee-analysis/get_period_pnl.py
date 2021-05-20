9e22b9ca277df457df2ac3bf39f5dd479333fac0
ajpwahqgbi <twabbiqjp@protonmail.com>
2021-05-20 03:26:06 +0000

    fee-analysis: Sort PnL entries by net earnings

--- fee-analysis/get_period_pnl.py
+++ fee-analysis/get_period_pnl.py
@@ -107,14 +107,18 @@ for forward in rpc.listforwards()["forwards"]:
         channel_fees_collected[out_scid] = fees_collected_chanout
 
 #report statistics for channels:
-print("──────────────┬───────────┬──────────────────────┬──────────────────────┬──────────────────────┬──────────┐")
-print("   channel    │ # in, out │      ksat moved      │    fees collected    │  rebalancing costs   │    net   │")
-print("══════════════╪═══════════╪══════════════════════╪══════════════════════╪══════════════════════╪══════════╡")
-for scid in sorted(channels, key = lambda s: msat_moved[s][0][1] + msat_moved[s][1][1] if s in msat_moved else 0):
+rows = []
+for scid in channels:
     collected_fees = channel_fees_collected[scid] if scid in channel_fees_collected else (0, 0)
     moved = msat_moved[scid] if scid in msat_moved else ((0, 0), (0, 0))
     rebalance_cost = channel_rebalance_paid[scid] if scid in channel_rebalance_paid else (0, 0)
     net_earnings = ((collected_fees[0] + collected_fees[1]) - (rebalance_cost[0] + rebalance_cost[1]))
-    print("%s │%4d, %4d │ %9.3f, %9.3f │ %9.3f, %9.3f │ %9.3f, %9.3f │ %8.3f │" % (scid.rjust(13), moved[0][0], moved[1][0], moved[0][1] / 1000000.0, moved[1][1] / 1000000.0, collected_fees[0] / 1000.0, collected_fees[1] / 1000.0, rebalance_cost[0] / 1000.0, rebalance_cost[1] / 1000.0, net_earnings / 1000.0))
+    rows.append((scid, moved[0][0], moved[1][0], moved[0][1] / 1000000.0, moved[1][1] / 1000000.0, collected_fees[0] / 1000.0, collected_fees[1] / 1000.0, rebalance_cost[0] / 1000.0, rebalance_cost[1] / 1000.0, net_earnings / 1000.0))
+
+print("──────────────┬───────────┬──────────────────────┬──────────────────────┬──────────────────────┬──────────┐")
+print("   channel    │ # in, out │      ksat moved      │    fees collected    │  rebalancing costs   │    net   │")
+print("══════════════╪═══════════╪══════════════════════╪══════════════════════╪══════════════════════╪══════════╡")
+for row in sorted(rows, key = lambda r: r[9]):
+    print("%s │%4d, %4d │ %9.3f, %9.3f │ %9.3f, %9.3f │ %9.3f, %9.3f │ %8.3f │" % (row[0].rjust(13), row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
 print("──────────────┴───────────┴──────────────────────┴──────────────────────┴──────────────────────┴──────────┘")
 
