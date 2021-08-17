f4c066449193138c488860bf84d4e681ab28f3a0
ajpwahqgbi <twabbiqjp@protonmail.com>
2021-08-17 23:00:39 +0000

    node_recommender: In analyze.py, smooth rank product with a logarithmic function.
    Also make the number of nodes reported in each category configurable as a command-line argument.

--- channel-analysis/node-recommender/analyze.py
+++ channel-analysis/node-recommender/analyze.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #Pipe input from node_recommender.py
 import sys, json
+import math
 from functools import reduce
 
 def trunc_pad(str_in, max_len):
@@ -13,6 +14,10 @@ def trunc_pad(str_in, max_len):
         ret += " "
     return ret
 
+def f(rank):
+    return 1.0 + (math.log(1.0 + rank) / math.log(8))
+
+n = 10 if len(sys.argv) == 1 else int(sys.argv[1])
 json_data = json.load(sys.stdin)
 cur_maxflow_geo = float(json_data["root_node_metrics"]["existing_maxflow_geomean"])
 cur_shortest_geo = float(json_data["root_node_metrics"]["existing_shortest_path_geomean"])
@@ -31,51 +36,52 @@ peer_products = dict()
 
 print("-----")
 print("")
-print("Top 10 potential peers by maxflow:")
+print("Top %d potential peers by maxflow:" % n)
 print("")
-for i in range(0, 10):
+for i in range(0, n):
     peer = peers_by_maxflow[i]
     print("%.4f - %s (%s)" % (float(peer["new_maxflow_geomean"]), trunc_pad(peer["peer_alias"], 30), peer["peer_id"]))
 for i in range(0, len(peers_by_maxflow)):
     peer = peers_by_maxflow[i]
     peer_id = peer["peer_id"]
-    peer_products[peer_id] = i + 1
+    peer_products[peer_id] = f(i)
 print("")
 
 print("-----")
 print("")
-print("Top 10 potential peers by shortest paths:")
+print("Top %d potential peers by shortest paths:" % n)
 print("")
-for i in range(0, 10):
+for i in range(0, n):
     peer = peers_by_shortest[i]
     print("%.4f - %s (%s)" % (float(peer["new_shortest_path_geomean"]), trunc_pad(peer["peer_alias"], 30), peer["peer_id"]))
 for i in range(0, len(peers_by_shortest)):
     peer = peers_by_shortest[i]
     peer_id = peer["peer_id"]
-    peer_products[peer_id] = peer_products[peer_id] * (i + 1)
+    peer_products[peer_id] = peer_products[peer_id] * f(i)
 print("")
 
 print("-----")
 print("")
-print("Top 10 potential peers by cheapest ppm:")
+print("Top %d potential peers by cheapest ppm:" % n)
 print("")
-for i in range(0, 10):
+for i in range(0, n):
     peer = peers_by_cheapest[i]
     print("%.4f - %s (%s)" % (float(peer["new_cheapest_ppm_geomean"]), trunc_pad(peer["peer_alias"], 30), peer["peer_id"]))
 for i in range(0, len(peers_by_cheapest)):
     peer = peers_by_cheapest[i]
     peer_id = peer["peer_id"]
-    peer_products[peer_id] = peer_products[peer_id] * (i + 1)
+    peer_products[peer_id] = peer_products[peer_id] * f(i)
 print("")
 
 print("-----")
 print("")
-print("Top 10 potential peers by rank product:")
+print("Top %d potential peers by f(rank) product:" % n)
+print("(where f() is some logarithmic function)")
 print("")
 peers_by_rank_product = sorted(peers, key = lambda x: peer_products[x["peer_id"]])
-for i in range(0, 10):
+for i in range(0, n):
     peer = peers_by_rank_product[i]
-    print("%7d - %s (%s)" % (peer_products[peer["peer_id"]], trunc_pad(peer["peer_alias"], 30), peer["peer_id"]))
+    print("%7.4f - %s (%s)" % (peer_products[peer["peer_id"]], trunc_pad(peer["peer_alias"], 30), peer["peer_id"]))
 print("")
 
 print("-----")
